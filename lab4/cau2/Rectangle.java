1edd594ac97d222428e23849c0c88879be3a524f
viet363 <110683284+viet363@users.noreply.github.com>
2023-03-11 15:09:28 +0700

    Add files via upload

---
+++ lab4/cau2/Rectangle.java
@@ -0,0 +1,42 @@
+package cau2;
+
+public class Rectangle extends Shape {
+    private double width = 1.0;
+    private double length = 1.0;
+
+    public Rectangle(String color, boolean filled, double width, double length) {
+        super(color, filled);
+        this.width = width;
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getLength() {
+        return this.length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getArea() {
+        return this.width * this.length;
+    }
+
+    public double getPerimeter() {
+        return (this.length + this.width) * 2;
+    }
+    @Override
+    public String toString() {
+        return "{" +
+                " width='" + getWidth() + "'" +", length='" + getLength() + "'" + "}";
+    }
+
+}
