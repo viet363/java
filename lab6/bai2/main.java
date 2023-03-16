bed503b4207ed800812db687d744a656c66ece4d
viet363 <110683284+viet363@users.noreply.github.com>
2023-03-16 20:23:44 +0700

    Update main.java

--- lab6/bai2/main.java
+++ lab6/bai2/main.java
@@ -1,7 +1,7 @@
 package lab6.bai2;
 public class main {
     public static void main(String[] args) {
-        HocSinh HS=new HocSinh(LQV, lop12A, 9, 9, 7);
+        HocSinh HS=new HocSinh("LQV", "lop12A", 9, 9, 7);
         System.out.println("diem trung binh ="+HS.DiemTrungBinh());
         HS =new HocSinhChuyenToan();
         System.out.println("diem trung binh ="+HS.DiemTrungBinh());
