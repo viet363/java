f9ec4e4bfa92194539300fcca70d97a9b63e595b
viet363 <110683284+viet363@users.noreply.github.com>
2023-03-16 13:30:12 +0700

    Delete lab4 directory

--- lab4/cau1/Cam.java
+++
@@ -1,9 +0,0 @@
-package cau1;
-
-public class Cam extends Hoaqua {
-    public Cam(){
-    }
-    public Cam(String nguongoc, int soluong, int giaban, int ngaynhap, float gianhap,String loai ){
-         super(loai, nguongoc, soluong, giaban, ngaynhap, gianhap);
-    }
-}
