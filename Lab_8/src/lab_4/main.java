39dc9a937e3cd991f5f2e347d2b80955fcd6a1e8
viet363 <110683284+viet363@users.noreply.github.com>
2023-04-13 20:54:29 +0700

    Add files via upload

---
+++ Lab_8/src/lab_4/main.java
@@ -0,0 +1,12 @@
+package lab_4;
+
+public class main {
+    public static void main(String[] args) {
+        SanPham sp1 = new SanPham();
+        SanPham sp2 = new SanPham();
+        sp1.nhap();
+        sp1.xuat();
+        sp2.nhap();
+        sp2.xuat();
+    }
+}
