dadc42571a11335b43948cd2ece367524ac01d56
viet363 <110683284+viet363@users.noreply.github.com>
2023-04-23 15:50:08 +0700

    Add files via upload

---
+++ khocaycanh/src/quanlykhocaycanh/listCayThanGo.java
@@ -0,0 +1,39 @@
+package quanlykhocaycanh;
+
+import java.util.Scanner;
+
+public class listCayThanGo extends CayCanh {
+
+    private double giathanh;
+
+    public listCayThanGo(double giathanh, String MaCC, String tencay, String phanloai, double soluong, int ngay, int thang, int nam) {
+        super(MaCC, tencay, phanloai, soluong, ngay, thang, nam);
+        this.giathanh = giathanh;
+    }
+
+    public listCayThanGo() {
+        super();
+    }
+
+    public double getgiathanh() {
+        return giathanh;
+    }
+
+    public void setgiathanh(double giathanh) {
+        this.giathanh = giathanh;
+    }
+
+    @Override
+    public void Nhap() {
+        super.Nhap();
+        Scanner input = new Scanner(System.in);
+        System.out.print("gia nhap vao cua cay : ");
+        giathanh = input.nextDouble();
+    }
+
+    @Override
+    public void Xuat() {
+        super.Xuat();
+        System.out.println("| Gia Nhap: " + getgiathanh() + "|");
+    }
+}
