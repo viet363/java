f006323997edc61174aaa79dbfe40a0754ae718e
viet363 <110683284+viet363@users.noreply.github.com>
2023-04-23 16:48:47 +0700

    Update listCayThanThao.java

--- khocaycanh/src/quanlykhocaycanh/listCayThanThao.java
+++ khocaycanh/src/quanlykhocaycanh/listCayThanThao.java
@@ -1,22 +1,37 @@
 package quanlykhocaycanh;
 
-public class listCayThanThao extends CayCanh {
+import java.util.Scanner;
 
-    public listCayThanThao(String MaCC, String tencay, String phanloai, double soluong, int ngay, int thang, int nam) {
+public class listCayThanThao extends CayCanh {
+     private double giathanh;
+
+    public listCayThanThao(double giathanh, String MaCC, String tencay, String phanloai, double soluong, int ngay, int thang, int nam) {
         super(MaCC, tencay, phanloai, soluong, ngay, thang, nam);
+        this.giathanh = giathanh;
     }
 
     public listCayThanThao() {
-        super();
+    }
+
+    public double getgiathanh() {
+        return giathanh;
+    }
+
+    public void setgiathanh(double giathanh) {
+        this.giathanh = giathanh;
     }
 
     @Override
     public void Nhap() {
         super.Nhap();
+         Scanner input = new Scanner(System.in);
+        System.out.print("gia nhap vao cua cay : ");
+        giathanh = input.nextDouble();
     }
 
     @Override
     public void Xuat() {
         super.Xuat();
+          System.out.println("| Gia Nhap: " + getgiathanh() + "|");
     }
 }
